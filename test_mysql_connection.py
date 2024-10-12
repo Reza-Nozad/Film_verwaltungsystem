e710c9f12507f2d797a6ecadc4b13c49813e72fb
reza <reza.nozad@gmail.com>
2024-10-12 18:05:11 +0000

    mysql_connected

---
+++ test_mysql_connection.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+def create_connection():
+    connection = mysql.connector.connect(
+          host='172.29.66.162',
+          user='Reza',
+          password='Renomodus2007!',
+          database='Filmverwaltungsystem'
+     )
+    return connection
+
+try:
+    conn = create_connection()
+    print("Verbindung zur MySQL-Datenbank erfolgreich")
+    conn.close()
+except mysql.connector.Error as err:
+    print(f"Fehler: {err}")
